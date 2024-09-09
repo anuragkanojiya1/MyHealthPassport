c7acf75839d5ac0b49c7e87d5fa058e6a0a643df
Anurag Kanojiya <anurag.kanojiya829@gmail.com>
2024-09-09 13:09:09 +0530

    Adding CosmocloudApi.kt and Data class for it

---
+++ app/src/main/java/com/example/myhealthpassport/cloud/Data.kt
@@ -0,0 +1,13 @@
+data class UploadRequestBody(
+    val name: String,
+    val size: Long
+)
+
+data class PresignedUrlResponse(
+    val url: String,
+    val fields: Map<String, String>
+)
+
+data class FileDownloadResponse(
+    val downloadUrl: String
+)
