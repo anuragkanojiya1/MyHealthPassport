137f5d4a7cc1c4439ed554835699545d8cf34de9
Anurag Kanojiya <anurag.kanojiya829@gmail.com>
2024-09-09 13:14:38 +0530

    Adding AnthropicApi and its RetrofitInstance

---
+++ app/src/main/java/com/example/myhealthpassport/network/AnthropicApi.kt
@@ -0,0 +1,20 @@
+package com.example.myhealthpassport.network
+
+import com.example.myhealthpassport.ANTHROPIC_API_KEY
+import com.example.openaiapichatbot.response.MessageRequest
+import com.example.openaiapichatbot.response.MessageResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface AnthropicApi {
+
+    @POST("v1/messages")
+    fun chatCompletion(
+        @Body messageRequest : MessageRequest,
+        @Header("x-api-key") xApiKey : String = "$ANTHROPIC_API_KEY",
+        @Header("anthropic-version")  version : String = "2023-06-01",
+        @Header("content-type") contentType : String = "application/json"
+        ) : Call<MessageResponse>
+}
--- app/src/main/java/com/example/myhealthpassport/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/myhealthpassport/network/RetrofitInstance.kt
@@ -1,7 +1,6 @@
 package com.example.myhealthpassport.network
 
-import com.example.myhealthpassport.BASE_URL
-import com.example.openaiapichatbot.network.AnthropicApi
+import com.example.myhealthpassport.BASE_URL_X
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,7 +8,7 @@ object RetrofitInstance {
 
     private fun getInstance(): Retrofit{
             return Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BASE_URL_X)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
