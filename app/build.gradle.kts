7e630a1a5b510210b5f8629dbe7d6185b1d1c32b
Anurag Kanojiya <anurag.kanojiya829@gmail.com>
2026-07-13 19:51:54 +0530

    feat: add AES and GCP encryption for medical data storage in Firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.myhealthpassport"
         minSdk = 26
         targetSdk = 35 
-        versionCode = 1
-        versionName = "1.0.2"
+        versionCode = 2
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
