c2ae361073cbe15e132671e89237d3d0e8c7e69b
Anurag Kanojiya <anurag.kanojiya829@gmail.com>
2026-08-07 22:21:23 +0530

    feat: implemented background processing, notification and testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.anuragkanojiya.myhealthpassport"
         minSdk = 26
         targetSdk = 35 
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
