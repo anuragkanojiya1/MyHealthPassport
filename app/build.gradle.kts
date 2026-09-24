af61e722cf8484ea7dd33b23015a79cb62d12ca8
Anurag Kanojiya <anurag.kanojiya829@gmail.com>
2026-09-25 01:25:46 +0530

    feat: implemented background processing, notification and testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 36
 
-        versionCode = 18
+        versionCode = 19
         versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
